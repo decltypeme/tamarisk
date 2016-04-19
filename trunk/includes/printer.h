9ab12b23e8926906b83d3d9657f5cd710acf5be7
decltypeme <islamfm@aucegypt.edu>
2016-04-19 13:05:35 +0200

    Breathe

--- trunk/includes/printer.h
+++ trunk/includes/printer.h
@@ -18,7 +18,7 @@ THIS IS NOT AN EVERY DAY HEADER. THIS IS A CODE SNIPPET. ONLY INCLUDE WHEN USING
 //THIS IS NOT A NEAT WAY I KNOW TO WRITE CODE, BUT I JUST WANTED TO DO IT QUICKLY. SORRY FOR HOLDING ON TO THE CAPS LOCK> IT IS ACTUALLY  ENJOYABLE.
 
 
-//#define STATE_STRING
+#define STATE_STRING
 
 
 #ifndef STATE_STRING
@@ -27,7 +27,6 @@ pr_info("[%d]\t\t%s\t\t%ld\n", task->pid, task->comm, task->state);
 
 #ifdef STATE_STRING
 {
-char cool_state_string[20];
 long int state_no = task->state;
 if(state_no == TASK_RUNNING)
     pr_info("[%d]\t\t%s\t\tTASK RUNNING\n", task->pid, task->comm);
@@ -35,7 +34,7 @@ else if(state_no == TASK_INTERRUPTIBLE)
         pr_info("[%d]\t\t%s\t\tTASK INTERRUPTIBLE\n", task->pid, task->comm);
 else if(state_no == TASK_UNINTERRUPTIBLE)
         pr_info("[%d]\t\t%s\t\tTASK UNINTERRUPTIBLE\n", task->pid, task->comm);
-else if(state_no == TASK_ZOMBIE)
+else if(state_no == 8)
         pr_info("[%d]\t\t%s\t\tTASK ZOMBIE\n", task->pid, task->comm);
 else if(state_no == TASK_STOPPED)
         pr_info("[%d]\t\t%s\t\tTASK STOPPED\n", task->pid, task->comm);
